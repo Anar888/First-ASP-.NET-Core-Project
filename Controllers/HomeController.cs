2575d1c3e8462ee6cf2f21a2bd2438b121f4db72
AnarMasiyev777 <72488405+AnarMasiyev777@users.noreply.github.com>
2021-12-21 23:41:29 +0400

    first task

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication21._12._21.Controllers
+{
+    public class HomeController:Controller
+    {
+        //public ActionResult Index()
+        //{
+        //    var result = new ContentResult();
+        //    result.Content = "homework is finished";
+        //    return result;
+            
+        //return Content("homework is finished");
+
+        //}
+
+
+        public ViewResult Index()
+        {
+            ViewResult view = new ViewResult();
+            view.ViewName = "index";
+            return view;
+        }
+        public ViewResult About()
+        {
+            //ViewResult view = new ViewResult();
+            //view.ViewName = "About";
+            return View();
+        }
+        public ActionResult Contact()
+        {
+           
+            return View();
+        }
+
+
+        public ActionResult Detail(int id)
+        {
+            ViewData["id"] = id;
+            ViewBag.Id = id;
+            //ViewBag.Id = 70;
+            TempData["id"] = id;
+            return View();
+        }
+        public JsonResult Json()
+        {
+            var result = new JsonResult(new { name = "Anar", surname = "Masiyev" });
+            return result;
+
+        }
+
+
+    }
+}
